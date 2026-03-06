84dda5699bb06396a918516fddc27d50ac538207
Gabriel Cardozo <134400952+Gabriel-Crdz@users.noreply.github.com>
2026-03-05 21:48:56 -0300

    Add files via upload

---
+++ 03-HashMap/Estado.java
@@ -0,0 +1,20 @@
+public class Estado {
+    private String nome;
+    private String regiao;
+
+    /* Getter, Setter nome */
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /* Getter, Setter regiao */
+    public String getRegiao() {
+        return regiao;
+    }
+    public void setRegiao(String regiao) {
+        this.regiao = regiao;
+    }
+}
---
+++ 03-HashMap/exercicio/Aeroporto.java
@@ -0,0 +1,27 @@
+public class Aeroporto {
+    private String nome;
+    private String cidade;
+    private int altitude;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getAltitude() {
+        return altitude;
+    }
+    public void setAltitude(int altitude) {
+        this.altitude = altitude;
+    }
+
+}
