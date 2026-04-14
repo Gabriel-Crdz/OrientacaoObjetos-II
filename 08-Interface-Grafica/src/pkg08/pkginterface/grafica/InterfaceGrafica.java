6fd5910bedc89fc793a695dc5de58c33ea70bfdf
Gabriel Cardozo <134400952+Gabriel-Crdz@users.noreply.github.com>
2026-04-13 21:54:13 -0300

    Add files via upload

---
+++ 08-Interface-Grafica/src/pkg08/pkginterface/grafica/InterfaceGrafica.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg08.pkginterface.grafica;
+
+import java.util.ArrayList;
+import model.DaoVeiculo;
+import model.Veiculo;
+import view.VeiculoView;
+
+/**
+ *
+ * @author gabriel
+ */
+public class InterfaceGrafica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DaoVeiculo daoVeiculo = new DaoVeiculo();
+        ArrayList<Veiculo> veiculos = daoVeiculo.listarTodos();
+        for(Veiculo v : veiculos){
+            System.out.println(v.getModelo() + " - " + v.getMarca());
+        }
+        
+        new VeiculoView().setVisible(true); // Chamando o view 
+    }
+    
+}
